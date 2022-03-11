0fc9db0489eb440a6ff77e4c1f057690c255d0ab
[jsh1304] <[jung3914a@gmail.com]>
2022-03-11 21:45:48 +0900

    220311_timer

--- app/src/main/java/jj/appproject/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/jj/appproject/a7minutesworkout/ExerciseActivity.kt
@@ -2,9 +2,16 @@ package jj.appproject.a7minutesworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import jj.appproject.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
+
+    private var restTimer: CountDownTimer? = null
+
+    private var restProgress = 0
+
     private var binding: ActivityExerciseBinding? = null
     // ActivityMainBinding이 아니라 ActivityExerciseBinding
 
@@ -23,5 +30,45 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed() // 뒤로가기 버튼 클릭
         }
+
+        setupRestView()
+    }
+
+    private fun setupRestView(){
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+
+    private fun setRestProgressBar(){
+        binding?.progressBar?.progress = restProgress
+
+        restTimer = object: CountDownTimer(10000, 1000){
+            override fun onTick(p0: Long) {
+                restProgress++
+                binding?.progressBar?.progress = 10 - restProgress
+                binding?.tvTimer?.text = (10 - restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(
+                    this@ExerciseActivity,
+                    "운동을 시작합니다",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        }.start()
+    }
+
+    public override fun onDestroy() { // restTimer가 null인지 확인
+        if(restTimer != null){
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        super.onDestroy()
+        binding = null
     }
 }
