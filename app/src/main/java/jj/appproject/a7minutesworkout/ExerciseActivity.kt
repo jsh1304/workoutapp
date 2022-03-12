905367160d8f59e70d07310e3006f96950fe6c43
[jsh1304] <[jung3914a@gmail.com]>
2022-03-12 21:41:39 +0900

    220312_Exercisetimer

--- app/src/main/java/jj/appproject/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/jj/appproject/a7minutesworkout/ExerciseActivity.kt
@@ -3,14 +3,17 @@ package jj.appproject.a7minutesworkout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import jj.appproject.a7minutesworkout.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
 
-    private var restTimer: CountDownTimer? = null
-
+    private var restTimer: CountDownTimer? = null // 운동 전 준비 시간
     private var restProgress = 0
+
+    private var exerciseTimer: CountDownTimer? = null // 운동 시간
+    private var exerciseProgress = 0
 
     private var binding: ActivityExerciseBinding? = null
     // ActivityMainBinding이 아니라 ActivityExerciseBinding
@@ -30,6 +33,7 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolbarExercise?.setNavigationOnClickListener {
             onBackPressed() // 뒤로가기 버튼 클릭
         }
+
 
         setupRestView()
     }
@@ -53,13 +57,43 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
+                 setupExerciseView() // 10초 끝 -> 운동 시작
+            }
+
+        }.start()
+    }
+
+    private fun setupExerciseView(){
+        binding?.flProgressBar?.visibility = View.INVISIBLE
+        binding?.tvTitle?.text = "운동 이름"
+        binding?.flExerciseView?.visibility = View.VISIBLE
+
+        if(exerciseTimer != null){
+            exerciseTimer?.cancel()
+            exerciseProgress = 0
+        }
+
+        setExerciseProgressBar()
+    }
+
+    private fun setExerciseProgressBar(){
+
+        binding?.progressBarExercise?.progress = exerciseProgress
+
+        exerciseTimer = object : CountDownTimer(30000, 1000){
+            override fun onTick(p0: Long) {
+                exerciseProgress++
+                binding?.progressBarExercise?.progress = 30 - exerciseProgress
+                binding?.tvTimerExercise?.text = (30 - exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
                 Toast.makeText(
                     this@ExerciseActivity,
-                    "운동을 시작합니다",
+                    "30초 완료되었습니다.\n다음 운동을 준비해주세요.",
                     Toast.LENGTH_SHORT
                 ).show()
             }
-
         }.start()
     }
 
